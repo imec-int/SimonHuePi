773a8a50a88dafc1a2eb111b866bab62bfdfe301
Sam Decrock <sam.decrock@gmail.com>
2013-07-21 05:27:24 +0100

    Connected Raspberry Buttons

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@ var express = require('express');
 var http = require('http')
 var path = require('path');
 var util = require('util');
+var fs = require('fs');
 
 var config = require('./config');
 var lights = require('./lights');
+
+var onoff, Gpio, led, blue, pink, orange;
 
 // *************************
 // *** In Game Variables ***
@@ -77,6 +80,11 @@ function resetGame(){
 }
 
 function initLights(){
+	// turn on all lights
+	lights.turnOnLight(config.colors.blue.lightid);
+	lights.turnOnLight(config.colors.orange.lightid);
+	lights.turnOnLight(config.colors.pink.lightid);
+
 	// set lights to right color
 	lights.setLight(config.colors.blue.lightid, config.colors.blue.hue, config.colors.blue.sat, null);
 	lights.setLight(config.colors.orange.lightid, config.colors.orange.hue, config.colors.orange.sat, null);
@@ -141,3 +149,73 @@ function timerStopped(){
 
 	resetGame();
 }
+
+
+// ********************
+// *** GPIO BUTTONS ***
+// ********************
+
+function watchGpio(){
+	var debounceTimeout = 240;
+
+	onoff = require('onoff');
+	Gpio = require('onoff').Gpio;
+	led = new Gpio(17, 'out');
+
+	blue = new Gpio(23, 'in', 'falling',{
+		debounceTimeout : debounceTimeout,
+		persistentWatch : true
+	});
+
+	pink = new Gpio(25, 'in', 'falling',{
+		debounceTimeout : debounceTimeout,
+		persistentWatch : true
+	});
+
+	orange = new Gpio(22, 'in', 'falling',{
+		debounceTimeout : debounceTimeout,
+		persistentWatch : true
+	});
+
+
+	blue.watch(function (err, value) {
+		if (err) return console.log(err);
+		if (value == 0) input('blue'); // 0 = pressed down
+	});
+	pink.watch(function (err, value) {
+		if (err) return console.log(err);
+		if (value == 0) input('pink'); // 0 = pressed down
+	});
+	orange.watch(function (err, value) {
+		if (err) return console.log(err);
+		if (value == 0) input('orange'); // 0 = pressed down
+	});
+
+	blink();
+	printStatus();
+}
+
+function blink(){
+	led.writeSync( 1 );
+	setTimeout(function(){
+		led.writeSync( 0 );
+	},300);
+}
+
+function printStatus(){
+	console.log("Current State:");
+	console.log("- blue: " + blue.readSync() );
+	console.log("- pink: " + pink.readSync() );
+	console.log("- orange: " + orange.readSync() );
+	console.log("==============");
+}
+
+// check if we're on the Raspberry Pi:
+fs.exists('/sys/class/gpio/', function (exists){
+	if(exists){
+		console.log("We're on the Raspberry Pi.. watching GPIOs");
+		watchGpio();
+	}else{
+		console.log("Looks like we're not on the Raspberry Pi.");
+	}
+});
