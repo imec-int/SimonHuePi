8078ad64254a148afde21605e9c870a13823c03f
Robby Wauters <robbywauters@gmail.com>
2013-07-21 02:34:19 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,24 @@
+exports.player2lightid = {
+	'm4tt' 		: 1,
+	'robby' 	: 4,
+	'crockysam' : 5
+};
+
+//flag.i is een beetje hacky :p
+exports.flags = {
+	A  : {code: 'A' , name:'Agility'					, hue: 60607, sat: 253},
+	CL : {code: 'CL', name:'Cloaking'					, hue: 61809, sat: 178}, // lichtroos
+	G  : {code: 'G' , name:'Genocide'					, hue: 65527, sat: 253},
+	GM : {code: 'GM', name:'Guided Missile'				, hue: 60607, sat: 253},
+	L  : {code: 'L' , name:'Laser'						, hue: 49218, sat: 253},
+	OO : {code: 'OO', name:'Oscillation Overthruster'	, hue: 54633, sat: 253},
+	F  : {code: 'F' , name:'Rapid Fire'					, hue: 25717, sat: 254},
+	R  : {code: 'R' , name:'Ricochet'					, hue: 29961, sat: 254},
+	SH : {code: 'SH', name:'Shield'						, hue: 41322, sat: 252},
+	SW : {code: 'SW', name:'Shock Wave'					, hue: 25717, sat: 254},
+	SR : {code: 'SR', name:'Steam Roller'				, hue: 29424, sat: 254},
+	SB : {code: 'SB', name:'Super Bullet'				, hue: 22543, sat: 251},
+	T  : {code: 'T' , name:'Tiny'						, hue: 8436 , sat: 252},
+	V  : {code: 'V' , name:'High Speed'					, hue: 15480, sat: 251},
+	WG : {code: 'WG', name:'Wings'						, hue: 22543, sat: 251}
+}
